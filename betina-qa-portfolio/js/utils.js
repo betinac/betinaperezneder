f93db74da06ef5c53ecb97571d9da54cb8e3868a
Betina Perez Neder <betinap@protonmail.com>
2025-02-22 13:17:52 -0400

    Add copy email to clipboard functionality

--- betina-qa-portfolio/js/utils.js
+++ betina-qa-portfolio/js/utils.js
@@ -17,3 +17,34 @@ function clickHamburgerMenu() {
     });
 }
 document.addEventListener('DOMContentLoaded', clickHamburgerMenu )
+
+/* Social email copy functionality */
+function copyToClipboard() {
+    const copyButton = document.querySelector('.copy-email');
+    const emailAddress = document.querySelector('.email-address').textContent;
+    const feedbackEl = document.querySelector('.copy-feedback');
+
+    copyButton.addEventListener('click', () => {
+      navigator.clipboard.writeText(emailAddress)
+        .then(() => {
+            feedbackEl.textContent = 'Copied!';
+            feedbackEl.style.opacity = '1';          
+            
+            /* Clear the feedback after 2 seconds*/
+            setTimeout(() => {
+                feedbackEl.style.opacity = '0';
+                setTimeout(() => feedbackEl.textContent = '', 300);
+            }, 2000);
+        })
+        .catch(err => {
+            console.error('Error copying text: ', err);
+            feedbackEl.textContent = 'Error copying';
+            feedbackEl.style.opacity = '1';
+            setTimeout(() => {
+                feedbackEl.style.opacity = '0';
+                setTimeout(() => feedbackEl.textContent = '', 300);
+            }, 2000);
+        });
+    });
+}
+document.addEventListener('DOMContentLoaded', copyToClipboard )
